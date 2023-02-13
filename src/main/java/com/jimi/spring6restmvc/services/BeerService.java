90f90edd41522c333cff10ef71750b2b98000f4e
jimmi187 <project.jimi@proton.me>
2023-02-13 15:24:29 +0100

    first commit

---
+++ src/main/java/com/jimi/spring6restmvc/services/BeerService.java
@@ -0,0 +1,12 @@
+package com.jimi.spring6restmvc.services;
+
+import com.jimi.spring6restmvc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+
+    List<Beer> listBeers();
+    Beer getBeerById(UUID id);
+}
