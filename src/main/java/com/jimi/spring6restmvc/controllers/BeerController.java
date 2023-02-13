f85008c088ff473d0fac7f01e6c41c4300552d60
jimmi187 <project.jimi@proton.me>
2023-02-13 16:46:21 +0100

    Test get by id in COntroller with mockMvc

--- src/main/java/com/jimi/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/jimi/spring6restmvc/controllers/BeerController.java
@@ -2,19 +2,18 @@ package com.jimi.spring6restmvc.controllers;
 
 import com.jimi.spring6restmvc.model.Beer;
 import com.jimi.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/beer")
 public class BeerController {
--- src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,23 +1,34 @@
 package com.jimi.spring6restmvc.controllers;
 
+import com.jimi.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
 
     @Test
-    void getBeerById() {
+    void getBeerById() throws Exception {
 
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
 
     }
 }
