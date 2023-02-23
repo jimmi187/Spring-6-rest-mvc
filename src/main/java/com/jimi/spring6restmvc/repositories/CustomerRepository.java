845daa739323b47e22e3c6ea233b5838aed99c3a
jimmy187 <project.jimi@proton.me>
2023-02-24 00:50:40 +0100

    Repos added

---
+++ src/main/java/com/jimi/spring6restmvc/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.jimi.spring6restmvc.repositories;
+
+import com.jimi.spring6restmvc.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
