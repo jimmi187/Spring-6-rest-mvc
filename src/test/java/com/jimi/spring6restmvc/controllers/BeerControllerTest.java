896ec9ce0c6c8fb53472153fec235cfebe6665e1
jimmy187 <project.jimi@proton.me>
2023-02-23 21:41:12 +0100

    BeerController not found exception added to be thrown

--- src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerTest.java
@@ -133,8 +133,9 @@ class BeerControllerTest {
         given(beerService.getBeerById(any(UUID.class))).willThrow(NotFoundException.class);
 
         mockMvc.perform(get(BeerController.BEER_PATH_ID, UUID.randomUUID()))
-                .andExpect(status().isNoContent());
+                .andExpect(status().isNotFound());
     }
+
 
     @Test
     void getBeerById() throws Exception {
