fee7a20061b80e9d449973e47e138aeaf0481749
jimmy187 <project.jimi@proton.me>
2023-02-23 21:47:46 +0100

    Added ExceptionController with @ControllerAdvice to make it a global exception handler, easy  to customize and use

---
+++ src/main/java/com/jimi/spring6restmvc/controllers/ExceptionController.java
@@ -0,0 +1,14 @@
+package com.jimi.spring6restmvc.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(){
+        return ResponseEntity.notFound().build();
+    }
+}
