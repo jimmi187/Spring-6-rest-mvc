28effbe5d747daebef96213c76a900ee1788e4ec
jimmy187 <project.jimi@proton.me>
2023-02-24 16:22:11 +0100

    foundById and NotFoundById integrations test added

--- src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package com.jimi.spring6restmvc.controllers;
 
+import com.jimi.spring6restmvc.entities.Beer;
 import com.jimi.spring6restmvc.model.BeerDTO;
 import com.jimi.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +22,20 @@ class BeerControllerIT {
     @Autowired
     BeerRepository beerRepository;
 
+    @Test
+    void testBeerIdNotFound(){
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById(){
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+    }
 
     @Test
     void testListBeer(){
