c26f9d9de45ddf805598ebe58da2ccd2f6462742
jimmy187 <project.jimi@proton.me>
2023-02-24 16:15:09 +0100

    Integrations test added for beer controller, with Rollbacks and Transactional

---
+++ src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerIT.java
@@ -0,0 +1,38 @@
+package com.jimi.spring6restmvc.controllers;
+
+import com.jimi.spring6restmvc.model.BeerDTO;
+import com.jimi.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController beerController;
+    @Autowired
+    BeerRepository beerRepository;
+
+
+    @Test
+    void testListBeer(){
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList(){
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
