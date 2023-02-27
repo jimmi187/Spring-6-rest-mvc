9f9eef602f7374a22f982b2298dfa31c06254422
jimmy187 <project.jimi@proton.me>
2023-02-27 15:44:40 +0100

    Jpa response message via custom exceptionHandler with test

--- src/main/java/com/jimi/spring6restmvc/controllers/CustomErrorController.java
+++ src/main/java/com/jimi/spring6restmvc/controllers/CustomErrorController.java
@@ -1,6 +1,8 @@
 package com.jimi.spring6restmvc.controllers;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +16,26 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException exception){
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+
+        if(exception.getCause().getCause() instanceof ConstraintViolationException){
+            ConstraintViolationException ve = (ConstraintViolationException) exception.getCause().getCause();
+
+            List errors = ve.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                        Map<String,String> errmap = new HashMap<>();
+                        errmap.put(constraintViolation.getPropertyPath().toString(),
+                                constraintViolation.getMessage());
+                        return errmap;
+                    }).collect(Collectors.toList());
+            return responseEntity.body(errors);
+        }
+        return responseEntity.build();
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception){
--- src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/com/jimi/spring6restmvc/controllers/BeerControllerIT.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
@@ -26,9 +27,11 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -59,11 +62,15 @@ class BeerControllerIT {
         Map<String, Object> beerMap = new HashMap<>();
         beerMap.put("beerName", "New Name12344556789Name12344556789Name12344556789Name12344556789Name12344556789Name12344556789");
 
-        mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
+        MvcResult mvcResult = mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beerMap)))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.length()", is(1)))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
 
          }
 
