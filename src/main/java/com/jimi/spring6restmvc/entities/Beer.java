7fd97d8c28e4a3ed012fe2a6b640ceb92cb81dc9
jimmy187 <project.jimi@proton.me>
2023-02-27 14:51:06 +0100

    JPA validation

--- src/main/java/com/jimi/spring6restmvc/entities/Beer.java
+++ src/main/java/com/jimi/spring6restmvc/entities/Beer.java
@@ -2,6 +2,9 @@ package com.jimi.spring6restmvc.entities;
 
 import com.jimi.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -25,10 +28,22 @@ public class Beer {
 
     @Version
     private Integer version;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(length = 50)
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/com/jimi/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/jimi/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.jimi.spring6restmvc.repositories;
 
 import com.jimi.spring6restmvc.entities.Beer;
+import com.jimi.spring6restmvc.model.BeerStyle;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,7 +20,13 @@ class BeerRepositoryTest {
     @Test
     void testSaveBeer(){
         Beer savedBeer = beerRepository.save(Beer.builder()
-                .beerName("my beer").build());
+                .beerName("my beer")
+                        .beerStyle(BeerStyle.LAGER)
+                        .upc("12312312312")
+                        .price(new BigDecimal("11.99"))
+                .build());
+
+        beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
     }
