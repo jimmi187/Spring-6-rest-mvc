c12bbb4ad1b60495d1cf68a9a555ee691967a1f1
jimmy187 <project.jimi@proton.me>
2023-02-27 14:55:22 +0100

    Validations before data base

--- src/test/java/com/jimi/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/jimi/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package com.jimi.spring6restmvc.repositories;
 
 import com.jimi.spring6restmvc.entities.Beer;
 import com.jimi.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,21 @@ class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerTooLong(){
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("my beer 12345678901234567890123456789012345678901234567890")
+                    .beerStyle(BeerStyle.LAGER)
+                    .upc("12312312312")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
+
+    }
 
     @Test
     void testSaveBeer(){
