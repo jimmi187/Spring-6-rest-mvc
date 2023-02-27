b9cf9b56d9cc74e0485ffbb4d1fb8d67d744446e
jimmy187 <project.jimi@proton.me>
2023-02-27 14:35:13 +0100

    CustomErrorController custom message

--- src/main/java/com/jimi/spring6restmvc/controllers/CustomErrorController.java
+++ src/main/java/com/jimi/spring6restmvc/controllers/CustomErrorController.java
@@ -1,15 +1,31 @@
 package com.jimi.spring6restmvc.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomErrorController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity handleBundErrors(MethodArgumentNotValidException exception){
-        return ResponseEntity.badRequest().body(exception.getBindingResult().getFieldError());
+    ResponseEntity handleBindErrors(MethodArgumentNotValidException exception){
+
+        List errorList = exception.getFieldErrors().stream()
+                .map(FieldError ->{
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(FieldError.getField(), FieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+
+
+        return ResponseEntity.badRequest().body(errorList);
     }
 }
