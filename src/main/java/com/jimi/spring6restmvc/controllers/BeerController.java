494d83e29d0067fb3c3cc3225fcd6395fe0a7414
jimmy187 <project.jimi@proton.me>
2023-04-14 23:42:12 +0200

    some minor tests

--- src/main/java/com/jimi/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/jimi/spring6restmvc/controllers/BeerController.java
@@ -23,6 +23,8 @@ public class BeerController {
 
     private final BeerService beerService;
 
+
+
     @PatchMapping(BEER_PATH_ID)
     public ResponseEntity updateBeerPatchById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beer){
 
@@ -81,3 +83,73 @@ public class BeerController {
     }
 
 }
+
+
+
+//    private final TesterService testerService;
+
+//    @PostMapping("/api/v1/tester")
+//    public ResponseEntity handlePostTester(@RequestBody TesterDTO dto) throws IOException, IllegalAccessException {
+//
+//        String currentDir = System.getProperty("user.dir");
+//
+//        String filment = makeFiler(dto);
+//
+//
+//        Path filePath = Paths.get(currentDir, "./src/scripts", "facts.rdf");
+//        System.out.println(filePath);
+//        FileWriter fileWriter = new FileWriter(filePath.toFile());
+//        fileWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+//                "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+//                "        xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n" +
+//                "        xmlns:xsd=\"http://www.w3.org/2001/XMLSchema#\"\n" +
+//                "        xmlns:lc=\"http://informatika.ftn.uns.ac.rs/legal-case.rdf#\">\n" +
+//                "    <lc:case rdf:about=\"http://informatika.ftn.uns.ac.rs/legal-case.rdf#case01\">\n" +
+//                "        <lc:name>case 01</lc:name>\n" +
+//                "        <lc:defendant>Cvele</lc:defendant>\n" +
+//
+//                filment +
+////                "\t<lc:ubistvo_na_mah>no</lc:ubistvo_na_mah>\n" +
+//
+//                "    </lc:case>\n" +
+//                "</rdf:RDF>");
+//        fileWriter.close();
+//
+//        System.out.println();
+//        System.out.println(dto.getName());
+//        System.out.println(dto.getId());
+//        System.out.println(dto.getStyle());
+//        System.out.println(dto.getVersion());
+//        System.out.println(dto.getSurname());
+//        TesterDTO savertester = testerService.saveNewTester(dto);
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Location", "/api/v1/tester/" + savertester.getId().toString());
+//
+//        return new ResponseEntity(headers, HttpStatus.CREATED);
+//    }
+//
+//    public String makeFiler(TesterDTO dto) throws IllegalAccessException {
+//        //                "\t<lc:ubistvo_na_mah>no</lc:ubistvo_na_mah>\n" +
+//
+//        Class cls = dto.getClass();
+//        Field[] fields = cls.getDeclaredFields();
+//        for(Field field : fields){
+//            field.setAccessible(true);
+//            if(field.getType().getName().equals("java.lang.String")){
+//                System.out.println("ovo je string");
+//                Object value = field.get(dto);
+//                if(value == null)
+//                if(value.equals("no") || value == null){
+//                    System.out.println("atribut: " + field.getName() + "\tvrednost: " + value + "\tno");
+//                }else{
+//                    System.out.println("atribut: " + field.getName() + "\tvrednost: " + value + "\tyes");
+//                }
+//            }
+//
+//
+//
+//        }
+//
+//        return "";
+//    }
